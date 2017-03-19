31e1c7325843d9623bd14fba29639c58e82c9839
zhouyg <zhou.y.g.890@gmail.com>
2017-03-19 22:48:14 +0800

    init

---
+++ bin/www.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zyg on 17/3/19.
+ */
+const app = require('../app.js');
+
+
---
+++ web/store/types.js
@@ -0,0 +1,19 @@
+/**
+ * Created by zyg on 17/3/2.
+ */
+
+module.exports = {
+  TEST:'testx',
+  TEST2:'test2',
+  FIND_PLAYER: '对手',
+
+  CHESS_ADD:'棋子添加',
+
+  CHESS_MOVE:'棋子移动',
+
+  CLICK_ON_GRID:'点击格子',
+
+  SELECT_CHESS:'选择棋子',
+
+  CLEAR_CHESS:'清除选择',
+};
